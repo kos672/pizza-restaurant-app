4fd9e5037d69f1a6f9e3b7e60c70884d9c0666ca
Konstantin Kuzmin <kocc95@mail.ru>
2018-01-03 23:03:51 +0100

    added application context config

---
+++ src/main/java/root/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package root.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "root")
+public class AppConfig {
+
+}
