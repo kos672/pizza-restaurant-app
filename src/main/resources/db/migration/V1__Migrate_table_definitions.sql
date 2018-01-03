9480727fb476fe7f28009758b4d7dca82bd5be5d
Konstantin Kuzmin <kocc95@mail.ru>
2018-01-03 23:02:18 +0100

    added database migration sql script

---
+++ src/main/resources/db/migration/V1__Migrate_table_definitions.sql
@@ -0,0 +1,145 @@
+CREATE TABLE address
+(
+  id_address INT PRIMARY KEY AUTO_INCREMENT,
+  id_client  INT,
+  city       VARCHAR(55) NOT NULL,
+  house_nr   VARCHAR(5)  NOT NULL,
+  flat_nr    SMALLINT    NOT NULL
+);
+
+CREATE TABLE category
+(
+  id_category   INT PRIMARY KEY AUTO_INCREMENT,
+  name_category VARCHAR(55) NOT NULL
+);
+
+CREATE TABLE client
+(
+  id_client    INT PRIMARY KEY AUTO_INCREMENT,
+  id_address   INT           NOT NULL,
+  name_client  VARCHAR(55)   NOT NULL,
+  phone_client NUMERIC(9, 0) NOT NULL
+);
+
+CREATE TABLE dish
+(
+  id_dish     INT PRIMARY KEY AUTO_INCREMENT,
+  id_category INT           NOT NULL,
+  name_dish   INT           NOT NULL,
+  price_dish  DECIMAL(2, 2) NOT NULL
+);
+
+CREATE TABLE drink
+(
+  id_drink    INT PRIMARY KEY AUTO_INCREMENT,
+  name_drink  VARCHAR(55)   NOT NULL,
+  price_drink DECIMAL(2, 2) NOT NULL
+);
+
+CREATE TABLE ingredient
+(
+  id_ingredient   INT PRIMARY KEY AUTO_INCREMENT,
+  name_ingredient VARCHAR(55) NOT NULL
+);
+
+CREATE TABLE dish_ingredient
+(
+  id_dish       INT NOT NULL,
+  id_ingredient INT NOT NULL,
+  PRIMARY KEY (id_dish, id_ingredient)
+);
+
+CREATE TABLE pay_method
+(
+  id_pay_method   INT PRIMARY KEY AUTO_INCREMENT,
+  name_pay_method VARCHAR(55) NOT NULL
+);
+
+CREATE TABLE purchase
+(
+  id_purchase   INT PRIMARY KEY AUTO_INCREMENT,
+  nr_purchase   INT NOT NULL,
+  extra_cheese  TINYINT,
+  time_purchase TIME,
+  notes         VARCHAR(255),
+  id_address    INT NOT NULL,
+  id_pay_method INT NOT NULL
+);
+
+CREATE TABLE purchase_dish
+(
+  id_purchase INT NOT NULL,
+  id_dish     INT NOT NULL,
+  count_dish  INT NOT NULL,
+  PRIMARY KEY (id_purchase, id_dish)
+);
+
+CREATE TABLE purchase_drink
+(
+  id_drink    INT NOT NULL,
+  id_purchase INT NOT NULL,
+  count_drink INT NOT NULL,
+  PRIMARY KEY (id_drink, id_purchase)
+);
+
+CREATE TABLE purchase_ingredient
+(
+  id_purchase   INT NOT NULL,
+  id_ingredient INT NOT NULL,
+  PRIMARY KEY (id_purchase, id_ingredient)
+);
+
+ALTER TABLE purchase_ingredient
+  COMMENT 'additional ingredient';
+
+ALTER TABLE address
+  ADD CONSTRAINT FK_ADDRESS_RELATIONS_CLIENT FOREIGN KEY (id_client)
+REFERENCES client (id_client);
+
+ALTER TABLE client
+  ADD CONSTRAINT FK_CLIENT_RELATIONS_ADDRESS FOREIGN KEY (id_address)
+REFERENCES address (id_address);
+
+ALTER TABLE dish
+  ADD CONSTRAINT FK_DISH_RELATIONS_CATEGORY FOREIGN KEY (id_category)
+REFERENCES category (id_category);
+
+ALTER TABLE dish_ingredient
+  ADD CONSTRAINT FK_DISH_ING_RELATIONS_DISH FOREIGN KEY (id_dish)
+REFERENCES dish (id_dish);
+
+ALTER TABLE dish_ingredient
+  ADD CONSTRAINT FK_DISH_ING_RELATIONS_INGREDIE FOREIGN KEY (id_ingredient)
+REFERENCES ingredient (id_ingredient);
+
+ALTER TABLE purchase
+  ADD CONSTRAINT FK_purchase_RELATIONS_PAY_METH FOREIGN KEY (id_pay_method)
+REFERENCES pay_method (id_pay_method);
+
+ALTER TABLE purchase
+  ADD CONSTRAINT FK_purchase_RELATIONS_ADDRESS FOREIGN KEY (id_address)
+REFERENCES address (id_address);
+
+ALTER TABLE purchase_dish
+  ADD CONSTRAINT FK_purchase_DI_RELATIONS_purchase FOREIGN KEY (id_purchase)
+REFERENCES purchase (id_purchase);
+
+ALTER TABLE purchase_dish
+  ADD CONSTRAINT FK_purchase_DI_RELATIONS_DISH FOREIGN KEY (id_dish)
+REFERENCES dish (id_dish);
+
+ALTER TABLE purchase_drink
+  ADD CONSTRAINT FK_purchase_DR_RELATIONS_DRINK FOREIGN KEY (id_drink)
+REFERENCES drink (id_drink);
+
+ALTER TABLE purchase_drink
+  ADD CONSTRAINT FK_purchase_DR_RELATIONS_purchase FOREIGN KEY (id_purchase)
+REFERENCES purchase (id_purchase);
+
+ALTER TABLE purchase_ingredient
+  ADD CONSTRAINT FK_purchase_IN_RELATIONS_purchase FOREIGN KEY (id_purchase)
+REFERENCES purchase (id_purchase);
+
+ALTER TABLE purchase_ingredient
+  ADD CONSTRAINT FK_purchase_IN_RELATIONS_INGREDIE FOREIGN KEY (id_ingredient)
+REFERENCES ingredient (id_ingredient);
