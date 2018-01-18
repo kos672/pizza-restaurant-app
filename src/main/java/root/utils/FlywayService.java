33dfa9eb455351054657ddafe8391220025fb62e
Konstantin Kuzmin <kocc95@mail.ru>
2018-01-19 00:05:54 +0100

    changed signature and functionality of flyway service

--- src/main/java/root/utils/FlywayService.java
+++ src/main/java/root/utils/FlywayService.java
@@ -1,25 +1,24 @@
 package root.utils;
 
 import org.flywaydb.core.Flyway;
-import org.springframework.stereotype.Service;
 
 /**
  * Class is istended for clean db schema
  */
-@Service
-public class FlywayCleanService {
+public class FlywayService {
 
-    private FlywayCleanService() {
+    private FlywayService() {
+    }
+
+    public static void main(String[] args) {
+        prepareFlyway().clean();
+        prepareFlyway().migrate();
     }
 
     private static Flyway prepareFlyway() {
         Flyway flyway = new Flyway();
         flyway.setDataSource("jdbc:mysql://localhost:3306/pizza_rest?useSSL=false", "root", "root");
         return flyway;
-    }
-
-    public static void main(String[] args) {
-        prepareFlyway().clean();
     }
 
 }
