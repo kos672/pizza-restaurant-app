ebd1aaec360a7aa779ed483f90082f14a72b8685
Konstantin <kosax22@gmail.com>
2018-05-30 16:59:38 +0200

    added possibility to receive cross-origin requests

--- src/main/java/root/business/dish/DishController.java
+++ src/main/java/root/business/dish/DishController.java
@@ -3,11 +3,13 @@ package root.business.dish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 @RestController("/dishes")
 public class DishController {
 
