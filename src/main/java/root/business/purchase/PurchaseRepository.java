ea497237545bc83afc982d3f528af86044dcf3a1
Konstantin <kocc95@mail.ru>
2018-06-05 23:22:21 +0200

    changes

---
+++ src/main/java/root/business/purchase/PurchaseRepository.java
@@ -0,0 +1,10 @@
+package root.business.purchase;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import root.entities.Purchase;
+
+@Repository
+public interface PurchaseRepository extends JpaRepository<Purchase, Integer> {
+
+}
