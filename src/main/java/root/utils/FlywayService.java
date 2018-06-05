a7f6b36bc46801f719e9cc9d2a56b86e787eee21
Konstantin <kocc95@mail.ru>
2018-06-05 23:12:55 +0200

    changed url in flyway service

--- src/main/java/root/utils/FlywayService.java
+++ src/main/java/root/utils/FlywayService.java
@@ -17,7 +17,9 @@ public class FlywayService {
 
     private static Flyway prepareFlyway() {
         Flyway flyway = new Flyway();
-        flyway.setDataSource("jdbc:mysql://localhost:3306/pizza_rest?useSSL=false", "root", "root");
+        //flyway.setDataSource("jdbc:mysql://localhost:3306/pizza_rest?useSSL=false", "root", "root");
+        flyway.setDataSource("jdbc:mysql://www.db4free.net:3306/pizza_rest?useSSL=false", "pizza_rest_admin",
+                "pizzarest");
         return flyway;
     }
 
